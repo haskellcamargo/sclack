d402a383411bb67af5e1ae7aeab43c6aaeb6cae5
Marcelo Camargo <marcelocamargo@linuxmail.org>
2018-05-26 16:43:22 -0300

    Playing with layouts

--- slack.py
+++ slack.py
@@ -5,22 +5,30 @@ from pyslack import config
 import pprint
 
 token = config.get_pyslack_config().get('DEFAULT', 'Token')
-slack = SlackClient(token)
-
-def exit_on_q(key):
-    if key in ('q', 'Q'):
-        raise urwid.ExitMainLoop()
+#slack = SlackClient(token)
 
 palette = [
     ('app', '', '', '', 'h99', 'h235'),
-    ('sidebar', '', '', '', 'white', 'h99')
+    ('sidebar', '', '', '', 'white', 'h99'),
+    ('chatbox', '', '', '', 'h99', 'h235'),
+    ('chatbox_header', '', '', '', 'h255', 'h235'),
+    ('message_input', '', '', '', 'h255', 'h234'),
+    ('prompt', '', '', '', 'h85', 'h234'),
+    ('datetime', '', '', '', 'h239', 'h235'),
+    ('username', '', '', '', 'h71,underline', 'h235'),
+    ('message', '', '', '', 'h253', 'h235'),
+    ('history_date', '', '', '', 'h244', 'h235')
 ]
 
 class Sidebar(urwid.BoxWidget):
     def __init__(self):
-        team = slack.api_call('team.info')['team']
-        channels = slack.api_call('channels.list', exclude_members=True, exclude_archived=True)['channels']
-        channels = list(filter(lambda channel: channel['is_member'], channels))
+        team = {'domain': 'workspace'}
+        # team = slack.api_call('team.info')['team']
+        # channels = slack.api_call('channels.list', exclude_members=True, exclude_archived=True)['channels']
+        channels = [
+            { 'name': 'compiler' },
+            { 'name': 'midgets-and-horses' }
+        ] # list(filter(lambda channel: channel['is_member'], channels))
         self.contents = urwid.SimpleListWalker([
             urwid.Text(' # ' + channel['name']) for channel in channels
         ])
@@ -34,13 +42,46 @@ class Sidebar(urwid.BoxWidget):
         self.listbox.keypress(size, key)
 
 def main():
+    urwid.set_encoding('UTF-8')
     sidebar = urwid.AttrWrap(Sidebar(), 'sidebar')
+    ## FRAME
+    header = urwid.AttrWrap(urwid.Pile([
+        urwid.Columns([
+            ('fixed', 1, urwid.Divider(u'─')),
+            ('fixed', 11, urwid.Text('#compiler', align='center')),
+            urwid.Divider(u'─')
+        ]),
+        urwid.Text(u' 👨 11 | Everything is terrible'),
+        urwid.Columns([
+            urwid.Divider(u'─'),
+            ('fixed', 14, urwid.Text(('history_date', 'Satuday 25th'), align='center')),
+            urwid.Divider(u'─')
+        ])
+    ]), 'chatbox_header')
+    footer = urwid.AttrWrap(urwid.LineBox(
+        urwid.Edit(('prompt', 'haskellcamargo> '), multiline=True)
+    ), 'message_input')
+
+    messages = [
+        urwid.Text([
+            ('datetime', '[22:10] '),
+            ('username', 'takanuva'),
+            ('message', ' sou javeiro ' + str(i))
+        ]) for i in range(1, 100)
+    ]
+
+    body = urwid.ListBox(urwid.SimpleListWalker(messages))
+    frame = urwid.Frame(body, header=header, footer=footer)
+    ##
+    chatbox = urwid.AttrWrap(frame, 'chatbox')
     columns = urwid.Columns([
-        ('fixed', 25, sidebar)
-    ], 1)
+        ('fixed', 25, sidebar),
+        ('weight', 1, chatbox)
+    ])
     app = urwid.Frame(urwid.AttrWrap(columns, 'app'))
-    loop = urwid.MainLoop(app, palette, unhandled_input=exit_on_q)
+    loop = urwid.MainLoop(app, palette)
     loop.screen.set_terminal_properties(colors=256)
+    loop.screen.set_mouse_tracking()
     loop.run()
 
 main()
